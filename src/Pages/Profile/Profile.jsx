14e11aa635f8dfe371ae46581441ad4ced74bf5d
niyaoe <niyas4334@gmail.com>
2026-02-24 22:49:08 +0530

    settings edit profile icons

--- src/Pages/Profile/Profile.jsx
+++ src/Pages/Profile/Profile.jsx
@@ -1,42 +1,50 @@
 import React from "react";
 import "./Profile.css";
+import { Link } from "react-router-dom";
+import Settings from "../Settings/Settings";
 
 const Profile = () => {
   return (
     <div className="profile-section">
       <div className="profile-card">
-
         {/* Top Section */}
-        <div className="profile-top">
+        <div className="outer-profile">
+          <div className="profile-top">
+            <img
+              src="https://i.pravatar.cc/150?img=12"
+              alt="profile"
+              className="profile-image"
+            />
 
-          <img
-            src="https://i.pravatar.cc/150?img=12"
-            alt="profile"
-            className="profile-image"
-          />
+            <div className="profile-info">
+              <h2 className="profile-username">sameer_fx</h2>
 
-          <div className="profile-info">
-            <h2 className="profile-username">sameer_fx</h2>
+              <div className="profile-stats">
+                <div className="stat">
+                  <span className="stat-number">54</span>
+                  <span className="stat-label">Posts</span>
+                </div>
 
-            <div className="profile-stats">
-              <div className="stat">
-                <span className="stat-number">54</span>
-                <span className="stat-label">Posts</span>
-              </div>
+                <div className="stat">
+                  <span className="stat-number">300</span>
+                  <span className="stat-label">Fans</span>
+                </div>
 
-              <div className="stat">
-                <span className="stat-number">300</span>
-                <span className="stat-label">Fans</span>
-              </div>
-
-              <div className="stat">
-                <span className="stat-number">143</span>
-                <span className="stat-label">Following</span>
+                <div className="stat">
+                  <span className="stat-number">143</span>
+                  <span className="stat-label">Following</span>
+                </div>
               </div>
             </div>
-
           </div>
-
+          <div className="profile-right">
+            <Link to="/" className="profile-settings-link">
+              <i className="bi bi-gear"></i>
+            </Link>
+            <Link to="/" className="profile-settings-link">
+              <i className="bi bi-pencil-square"></i>
+            </Link>
+          </div>
         </div>
 
         {/* Bio Section */}
@@ -45,7 +53,6 @@ const Profile = () => {
           <p>Adobe AfterEffects</p>
           <p>CapCut</p>
         </div>
-
       </div>
     </div>
   );
