9a30e5a06e9b4e8e81d4e513e803c16e017ea051
niyaoe <niya4334@gmail.com>
2026-02-25 16:21:19 +0530

    chat

--- src/ChatCard/ChatMessage.jsx
+++ src/ChatCard/ChatMessage.jsx
@@ -1,21 +1,25 @@
 import React from "react";
 import "./ChatMessage.css";
 
-export default function ChatMessage({ user, message, avatar }) {
+export default function ChatMessage({ user, message, avatar ,time }) {
   return (
     <div className="rf-chat-card">
       {/* DP */}
-      <img
-        src={avatar}
-        alt="user"
-        className="rf-chat-avatar"
-      />
+      <img src={avatar} alt="user" className="rf-chat-avatar" />
 
       {/* Message Content */}
-      <div className="rf-chat-content">
-        <span className="rf-chat-username">{user}</span>
-        <p className="rf-chat-text">{message}</p>
+      <div className="chat-outer">
+        <div className="rf-chat-content">
+          <span className="rf-chat-username">{user}</span>
+          <p className="rf-chat-text">{message}</p>
+        </div>
+        <div className="chat-time">
+          <p>{time}</p>
+        </div>
       </div>
     </div>
   );
 }
+
+
+
--- src/Pages/Feed_Section/Chat/GlobalChat.jsx
+++ src/Pages/Feed_Section/Chat/GlobalChat.jsx
@@ -8,12 +8,114 @@ export default function GlobalChat() {
       user: "sameer_fx",
       message: "Best render settings for reels?",
       avatar: "https://i.pravatar.cc/100?img=12",
+      time: "10:08pm",
     },
     {
       id: 2,
       user: "amal.mp4",
       message: "Use H264 + high bitrate 🔥",
       avatar: "https://i.pravatar.cc/100?img=5",
+      time: "10:08pm",
+    },
+    {
+      id: 3,
+      user: "sameer_fx",
+      message: "Best render settings for reels?",
+      avatar: "https://i.pravatar.cc/100?img=12",
+      time: "10:08pm",
+    },
+    {
+      id: 4,
+      user: "amal.mp4",
+      message: "Use H264 + high bitrate 🔥",
+      avatar: "https://i.pravatar.cc/100?img=5",
+      time: "10:08pm",
+    },
+    {
+      id: 5,
+      user: "sameer_fx",
+      message: "Best render settings for reels?",
+      avatar: "https://i.pravatar.cc/100?img=12",
+      time: "10:08pm",
+    },
+    {
+      id: 6,
+      user: "amal.mp4",
+      message: "Use H264 + high bitrate 🔥",
+      avatar: "https://i.pravatar.cc/100?img=5",
+      time: "10:08pm",
+    },
+    {
+      id: 7,
+      user: "sameer_fx",
+      message: "Best render settings for reels?",
+      avatar: "https://i.pravatar.cc/100?img=12",
+      time: "10:08pm",
+    },
+    {
+      id: 8,
+      user: "amal.mp4",
+      message: "Use H264 + high bitrate 🔥",
+      avatar: "https://i.pravatar.cc/100?img=5",
+      time: "10:08pm",
+    },
+    {
+      id: 9,
+      user: "sameer_fx",
+      message: "Best render settings for reels?",
+      avatar: "https://i.pravatar.cc/100?img=12",
+      time: "10:08pm",
+    },
+    {
+      id: 10,
+      user: "amal.mp4",
+      message: "Lorem ipsum dolor sit amet consectetur.",
+      avatar: "https://i.pravatar.cc/100?img=5",
+      time: "10:08pm",
+    },
+    {
+      id: 11,
+      user: "sameer_fx",
+      message: "Best render settings for reels?",
+      avatar: "https://i.pravatar.cc/100?img=12",
+      time: "10:08pm",
+    },
+    {
+      id: 12,
+      user: "amal.mp4",
+      message:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis nostrum corrupti doloribus eaque ducimus. Labore tenetur at repudiandae aspernatur adipisci nostrum deserunt totam, repellendus vitae autem suscipit odit nam nobis quisquam voluptatum.",
+      avatar: "https://i.pravatar.cc/100?img=5",
+      time: "10:08pm",
+    },
+    {
+      id: 13,
+      user: "sameer_fx",
+      message: "Best render settings for reels?",
+      avatar: "https://i.pravatar.cc/100?img=12",
+      time: "10:08pm",
+    },
+    {
+      id: 14,
+      user: "amal.mp4",
+      message: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cum, consequuntur.",
+      avatar: "https://i.pravatar.cc/100?img=5",
+      time: "10:08pm",
+    },
+    {
+      id: 15,
+      user: "sameer_fx",
+      message:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni nisi ducimus aspernatur a sapiente eius, ipsa amet possimus, consectetur architecto maiores hic aperiam dolores eum! At excepturi quas corrupti praesentium, ipsam totam iure saepe sunt laudantium, blanditiis, tenetur repellendus ipsum? Consequuntur expedita eos quae quis adipisci saepe doloribus exercitationem consequatur.",
+      avatar: "https://i.pravatar.cc/100?img=12",
+      time: "10:08pm",
+    },
+    {
+      id: 16,
+      user: "amal.mp4",
+      message: "Use H264 + high bitrate 🔥",
+      avatar: "https://i.pravatar.cc/100?img=5",
+      time: "10:08pm",
     },
   ];
 
@@ -32,3 +134,6 @@ export default function GlobalChat() {
     </div>
   );
 }
+
+
+
